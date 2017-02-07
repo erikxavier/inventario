23b41ca4a565e3978d66ecb53ca67d2a87d056ec
Erik Xavier <erik.xam@gmail.com>
2017-02-07 14:56:30 -0200

    Movimentar

--- app/movimentar/movimentar.controller.js
+++ app/movimentar/movimentar.controller.js
@@ -2,5 +2,32 @@ module.exports = MovimentarController
 
 MovimentarController.$inject = []
 function MovimentarController() {
-    
+    let vm = this;
+
+    vm.itens = [
+        "HD",
+        "Pen Drive",
+        "Mouse USB",
+        "Processador 1055",
+        "Memória RAM DDR3 4GB",
+        "Teclado USB",
+        "Cabo USB para Impressora",        
+
+    ]
+
+    vm.selectItem = function(item) {
+        vm.selectedItem = item;
+    }
+
+    vm.adicionaItemLista = function(item) {
+        vm.movimento.lista.push(item)        
+        vm.itens.splice(vm.itens.indexOf(item),1)
+    }
+
+    vm.novoMovimento = function() {
+        vm.movimento = {
+            data: new Date(),
+            lista: []
+        }
+    }
 }
